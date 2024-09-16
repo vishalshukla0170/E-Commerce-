f68660bf526c81b486d181492fb6a74a50104e0f
vishalshukla0170 <vishalshukla0170@gmail.com>
2024-09-16 17:34:57 +0530

    Update Products.js

--- src/data/Products.js
+++ src/data/Products.js
@@ -2,49 +2,49 @@ const products = [
   {
     id: 1,
     name: "Product 1",
-    price: "$10.00",
+    price: "1000.00 INR",
     image: "https://via.placeholder.com/150"
   },
   {
     id: 2,
     name: "Product 2",
-    price: "$20.00",
+    price: "200.00 INR",
     image: "https://via.placeholder.com/150"
   },
   {
     id: 3,
     name: "Product 3",
-    price: "$30.00",
+    price: "5000.00 INR",
     image: "https://via.placeholder.com/150"
   },
   {
     id: 4,
     name: "Product 4",
-    price: "$40.00",
+    price: "3000.00 INR",
     image: "https://via.placeholder.com/150"
   },
   {
     id: 5,
     name: "Product 5",
-    price: "$50.00",
+    price: "1350.00 INR",
     image: "https://via.placeholder.com/150"
   },
   {
     id: 6,
     name: "Product 6",
-    price: "$60.00",
+    price: "4379.00 INR",
     image: "https://via.placeholder.com/150"
   },
   {
     id: 7,
     name: "Product 7",
-    price: "$60.00",
+    price: "4550.00 INR",
     image: "https://via.placeholder.com/150"
   },
   {
     id: 8,
     name: "Product 8",
-    price: "$60.00",
+    price: "39584.00 INR",
     image: "https://via.placeholder.com/150"
   },
  
